6f06300aa18bb7e91612480b0ddcbb371a7c7d3c
SeungUkLee <lsy931106@gmail.com>
2019-06-05 20:30:54 +0900

    fixed ses sendmail timout problem - #10

--- ironmental-backend/src/router/sub/sub.ctrl.js
+++ ironmental-backend/src/router/sub/sub.ctrl.js
@@ -2,87 +2,58 @@ import Subscriber from 'database/models/subscriber'
 import uuidv4 from 'uuid/v4'
 import sendMail from 'lib/sendMail'
 
+const createRequest = (msg, isSub, isCertify) => {
+    return {
+        message: msg,
+        isSub: isSub,
+        isCertify: isCertify
+    }
+}
 
-export const subscribe = async (req, res) => {    
-    console.log(`req.body.email: ${req.body.email}`)
-    try {
-        await sendMail({
-            to: req.body.email,
-            subject: "Ironmental 구독인증 메일",
-            body: "<html><body>helloworld</body></html>"
+const createMailForm = (email, subject, html) => {
+    return {
+        email: email,
+        subject: subject,
+        html: html
+    }
+}
+
+export const subscribe = async (req, res) => {
+    const { email } = req.body
+    const subscriber = await Subscriber.findOne({ email: email })
+    const uuid_v4 = uuidv4()
+    const confirmLink = `https://ironmental.net/auth/confirm/${uuid_v4}`
+    const html = `<p><a href="mailto:${email}">${email}</a>님 반갑습니다!<br></p>이메일 인증을 하고 다음주 월요일부터 구독을 하고 싶으시다면 <a href="${confirmLink}">여기</a>를 눌러주세요!</p>`
+    
+    if (!subscriber) {
+        const sub = new Subscriber({
+            email: email,
+            confirmCode: uuid_v4,
+            isCertify: false
         })
-    } catch (e) {
-        res.send({ message: e })
+
+        // console.log(`send mail to ${req.body.email}`)
+        await sendMail(createMailForm(email, "Ironmental 구독인증 메일", html))
+        
+        await sub.save()
+        
+        return res.send(createRequest("인증메일을 보냈으니 확인해주세요 :)", false, false))
+
+    }
+    // console.log(`subscriber.isCertify: ${subscriber.isCertify}`)
+    let isCertify = subscriber.isCertify
+    if (isCertify) {
+        return res.send(createRequest("이미 구독자입니다 :D", true, subscriber.isCertify))
     }
     
-    return res.send({ message: "ok" })
+    await sendMail(createMailForm(email, "Ironmental 구독인증 메일", html))
+    
+    await Subscriber.findOneAndUpdate({ email: email }, { confirmCode: uuid_v4 }, { runValidators: true })
+    
+    res.send(createRequest("인증되지 않은 구독자입니다. 인증메일을 보냈으니 확인해주세요 :)", true, subscriber.isCertify))
 }
 
 export const show = async (req, res) => {
     const subscribers = await Subscriber.find()
     res.send(subscribers)
 }
-
-// import Subscriber from 'database/models/subscriber'
-// import uuidv4 from 'uuid/v4'
-// import sendMail from 'lib/sendMail'
-
-
-// export const subscribe = async (req, res) => {
-//     const subscriber = await Subscriber.findOne({ email: req.body.email })
-//     const uuid_v4 = uuidv4()
-//     const confirmLink = `https://ironmental.net/auth/confirm/${uuid_v4}`
-//     const html = `<p><a href="mailto:${subscriber}>${subscriber}</a>님 반갑습니다!<br></p>이메일 인증을 하고 다음주 월요일부터 구독을 하고 싶으시다면 <a href="${confirmLink}>여기</a>를 눌러주세요!</p>`
-    
-//     console.log(subscriber)
-//     if (!subscriber) {
-//         const sub = new Subscriber({
-//             email: req.body.email,
-//             confirmCode: uuid_v4,
-//             isCertify: false
-//         })
-//         console.log(`wait`)
-//         await sendMail({
-//             email: req.body.email,
-//             subject: "Ironmental 구독인증 메일",
-//             html: `<html><body>hello</body></html>`
-//         })
-//         console.log(`send mail to ${req.body.email}`)
-//         await sub.save()
-        
-//         return res.send({
-//             message: "인증메일을 보냈으니 확인해주세요 :)",
-//             isSub: false,
-//             isCertify: false
-//         })
-
-//     }
-
-//     let isCertify = subscriber.isCertify
-//     if (isCertify) {
-//         return res.send({
-//             message: "이미 구독자입니다 :D",
-//             isSub: true,
-//             isCertify: subscriber.isCertify
-//         })
-//     }
-//     console.log(`subscriber.email: ${subscriber.email}`)
-//     await sendMail({
-//         email: subscriber.email,
-//         subject: "Ironmental 구독인증 메일",
-//         html: `<html><body>hello</body></html>`
-//     })
-//     console.log(`Send mail ~`)
-//     await Subscriber.findOneAndUpdate({ email: req.body.email }, { confirmCode: uuid_v4 }, { runValidators: true })
-//     res.send({
-//         message: "인증되지 않은 구독자입니다. 인증메일을 보냈으니 확인해주세요 :)",
-//         isSub: true,
-//         isCertify: subscriber.isCertify
-//     })
-
-// }
-
-// export const show = async (req, res) => {
-//     const subscribers = await Subscriber.find()
-//     res.send(subscribers)
-// }
