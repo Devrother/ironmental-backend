90d6948ef9d545d908d76679c6569b90a9ee8e27
SeungUkLee <lsy931106@gmail.com>
2019-06-06 04:05:44 +0900

    Refactoring: extract mail message and html etc

--- ironmental-backend/src/router/sub/sub.ctrl.js
+++ ironmental-backend/src/router/sub/sub.ctrl.js
@@ -1,30 +1,26 @@
 import Subscriber from 'database/models/subscriber'
 import uuidv4 from 'uuid/v4'
 import sendMail from 'lib/sendMail'
+import { MAIL_SUBJECT, CHK_MAIL_MSG, NO_CERTIFY_MSG, ALREDY_SUB_MSG } from 'messages/strings'
+import { authMailHtml } from 'messages/htmlMail'
 
-const createRequest = (msg, isSub, isCertify) => {
-    return {
-        message: msg,
-        isSub: isSub,
-        isCertify: isCertify
-    }
+const { URL } = process.env
+
+const createRequest = (message, isSub, isCertify) => {
+    return { message, isSub, isCertify }
 }
 
 const createMailForm = (email, subject, html) => {
-    return {
-        email: email,
-        subject: subject,
-        html: html
-    }
+    return { email, subject, html }
 }
 
 export const subscribe = async (req, res) => {
     const { email } = req.body
-    const subscriber = await Subscriber.findOne({ email: email })
     const uuid_v4 = uuidv4()
-    const confirmLink = `https://ironmental.net/auth/confirm/${uuid_v4}`
-    const html = `<p><a href="mailto:${email}">${email}</a>님 반갑습니다!<br></p>이메일 인증을 하고 다음주 월요일부터 구독을 하고 싶으시다면 <a href="${confirmLink}">여기</a>를 눌러주세요!</p>`
-    
+    const confirmLink = `${URL}/auth/confirm/${uuid_v4}`
+    const html = authMailHtml(email, confirmLink)
+
+    const subscriber = await Subscriber.findOne({ email: email })
     if (!subscriber) {
         const sub = new Subscriber({
             email: email,
@@ -32,25 +28,22 @@ export const subscribe = async (req, res) => {
             isCertify: false
         })
 
-        // console.log(`send mail to ${req.body.email}`)
-        await sendMail(createMailForm(email, "Ironmental 구독인증 메일", html))
-        
+        await sendMail(createMailForm(email, MAIL_SUBJECT, html))
         await sub.save()
         
-        return res.send(createRequest("인증메일을 보냈으니 확인해주세요 :)", false, false))
-
+        return res.send(createRequest(CHK_MAIL_MSG, false, false))
     }
-    // console.log(`subscriber.isCertify: ${subscriber.isCertify}`)
+    
     let isCertify = subscriber.isCertify
+
     if (isCertify) {
-        return res.send(createRequest("이미 구독자입니다 :D", true, subscriber.isCertify))
+        return res.send(createRequest(ALREDY_SUB_MSG, true, isCertify))
     }
     
-    await sendMail(createMailForm(email, "Ironmental 구독인증 메일", html))
-    
+    await sendMail(createMailForm(email, MAIL_SUBJECT, html))
     await Subscriber.findOneAndUpdate({ email: email }, { confirmCode: uuid_v4 }, { runValidators: true })
     
-    res.send(createRequest("인증되지 않은 구독자입니다. 인증메일을 보냈으니 확인해주세요 :)", true, subscriber.isCertify))
+    res.send(createRequest(NO_CERTIFY_MSG, true, isCertify))
 }
 
 export const show = async (req, res) => {
