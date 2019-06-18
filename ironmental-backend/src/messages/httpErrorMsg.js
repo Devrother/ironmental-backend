5ab89a21ab9e9e71bba8ddaaaac0d838530d7472
SeungUkLee <lsy931106@gmail.com>
2019-06-19 05:00:58 +0900

    Edit: 404 err msg

--- ironmental-backend/src/messages/httpErrorMsg.js
+++ ironmental-backend/src/messages/httpErrorMsg.js
@@ -8,5 +8,5 @@ const createMsgForm = (status, message) => {
 }
 
 
-export const HTTP_404_MSG = createMsgForm(404, "Bad Request")
+export const HTTP_404_MSG = createMsgForm(404, "Not Found")
 export const HTTP_500_MSG = createMsgForm(500, "Internal Server Error")
