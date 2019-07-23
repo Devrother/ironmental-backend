71e59351f28aaec88ebda62e4b36f039867031db
SeungUkLee <lsy931106@gmail.com>
2019-07-24 02:21:19 +0900

    Add: custom erros

--- src/middlewares/errorResponse.js
+++ src/middlewares/errorResponse.js
@@ -1,16 +1,32 @@
 import mongoose from 'mongoose';
-import { HTTP_404_MSG, HTTP_500_MSG, HTTP_400_MSG } from 'messages/httpErrorMsg';
+import {
+  NotFoundError,
+  ValidationError,
+  UnauthorizedError
+} from 'lib/errors'
+import {
+  HTTP_400_MSG,
+  HTTP_403_MSG,
+  HTTP_404_MSG,
+  HTTP_500_MSG,
+} from 'messages/httpErrorMsg';
+import {
+  SERVER_ERROR_MSG
+} from 'messages/strings'
 
-const { DocumentNotFoundError, CastError } = mongoose.Error;
+const { CastError } = mongoose.Error;
 
 export default (err, req, res, next) => {
+  const { message } = err;
   switch (true) {
-    case err instanceof DocumentNotFoundError:
     case err instanceof CastError:
-      return res.status(404).send(HTTP_404_MSG);
-    case err instanceof Error && err.message === 'ValidationError':
-      return res.status(400).send(HTTP_400_MSG);
+    case err instanceof NotFoundError:
+      return res.status(404).send(HTTP_404_MSG(message));
+    case err instanceof UnauthorizedError:
+      return res.status(403).send(HTTP_403_MSG(message));
+    case err instanceof ValidationError:
+      return res.status(400).send(HTTP_400_MSG(message));
     default:
-      return res.status(500).send(HTTP_500_MSG);
+      return res.status(500).send(HTTP_500_MSG(SERVER_ERROR_MSG))
   }
 };
