5ac369acad68a3ad57c922f40bd6867b254b31fa
SeungUkLee <lsy931106@gmail.com>
2019-11-07 16:46:52 +0900

    Add: search - featrue #62

--- src/router/v1/interview/interview.ctrl.js
+++ src/router/v1/interview/interview.ctrl.js
@@ -8,19 +8,21 @@ import {
 } from 'transformer/interviewTransformer';
 
 export const listInterviews = async (req, res) => {
-  const { tag, limit, offset } = req.query;
+  const { tag, limit, offset, search } = req.query;
 
+  const searchQuery = search ? search.trim() : '';
   const tagName = tag ? tag.toLowerCase() : 'all';
   const limitNum = parseInt(limit, 10) || 4;
   const offsetNum = parseInt(offset, 10) || 0;
 
   const interviews = tagName === 'all'
-    ? await Interview.findWithPagination(offsetNum, limitNum)
+    ? await Interview.findWithPagination(offsetNum, limitNum, searchQuery)
     : (await Tag.joinInterviewsByName(tagName, limitNum, offsetNum)).interviews;
 
   const total = tagName === 'all'
-    ? await Interview.getInterviewsCnt()
+    ? await Interview.getInterviewsCnt(searchQuery)
     : await Tag.getInterviewsCntInTag(tagName);
+
   res.send(
     interviewListTransform(interviews, {
       tagName, limitNum, offsetNum, total,
